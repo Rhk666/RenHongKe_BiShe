2ad94cefb939d3a17a6bcaaa1fbcf153578af80f
Rhk666 <1906929347@qq.com>
2026-01-22 13:47:31 +0800

    Delete Test directory

--- Test/MDK-ARM/Test/mqttkit.d
+++
@@ -1,6 +0,0 @@
-test\mqttkit.o: ..\Core\Src\MqttKit.c
-test\mqttkit.o: ..\Core\Src\MqttKit.h
-test\mqttkit.o: ..\Core\Src\Common.h
-test\mqttkit.o: D:\keil\MSP\setup\KEIL\ARM\ARMCC\Bin\..\include\stdlib.h
-test\mqttkit.o: D:\keil\MSP\setup\KEIL\ARM\ARMCC\Bin\..\include\string.h
-test\mqttkit.o: D:\keil\MSP\setup\KEIL\ARM\ARMCC\Bin\..\include\stdio.h
